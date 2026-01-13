b1bb2f9ea61a10d3bce7f910f637ab634aecbd05
Dave Gaeddert <dave.gaeddert@gmail.com>
2026-01-13 10:48:15 -0600

    Update plain packages and migrate to AuthView
    Replace AuthViewMixin with AuthView following plain-auth 0.24.0 upgrade
    instructions. The new AuthView class inherits from SessionView, so
    template views now use AuthView + TemplateView composition.

--- app/views.py
+++ app/views.py
@@ -1,14 +1,14 @@
 import sys
 
-from plain.auth.views import AuthViewMixin
+from plain.auth.views import AuthView
 from plain.passwords.views import PasswordLoginView, PasswordSignupView
 from plain.runtime import __version__
 from plain.views import TemplateView
 
 
-# An example of a base mixin that can be used on almost all app views,
+# An example of a base view that can be used on almost all app views,
 # to require login, set HTML title, and share other common functionality.
-class BaseViewMixin(AuthViewMixin):
+class BaseView(AuthView, TemplateView):
     html_title = ""
 
     def get_template_context(self):
@@ -17,11 +17,10 @@ class BaseViewMixin(AuthViewMixin):
         return context
 
     def get_html_title(self):
-        """Override this method to set the HTML title dynamically."""
         return self.html_title
 
 
-class IndexView(BaseViewMixin, TemplateView):
+class IndexView(BaseView):
     template_name = "index.html"
     html_title = "Home"
     login_required = False
@@ -34,19 +33,19 @@ class IndexView(BaseViewMixin, TemplateView):
         return context
 
 
-class ExamplePrivateView(BaseViewMixin, TemplateView):
+class ExamplePrivateView(BaseView):
     template_name = "private.html"
     html_title = "Private"
     login_required = True
 
 
-class LoginView(BaseViewMixin, PasswordLoginView):
+class LoginView(BaseView, PasswordLoginView):
     template_name = "login.html"
     login_required = False
     html_title = "Log in"
 
 
-class SignupView(BaseViewMixin, PasswordSignupView):
+class SignupView(BaseView, PasswordSignupView):
     template_name = "signup.html"
     html_title = "Sign up"
     login_required = False
